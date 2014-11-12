a2c3d9aeaf7bb808830eadf7834ddd2af564c64f
nicholasbutlin <nicholasbutlin@gmail.com>
2014-11-12 13:10:16 +0000

    Added SEO and Spiderable

--- client/application/tracking.js
+++ client/application/tracking.js
@@ -3,5 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-54440316-1', 'auto');
+ga('create', 'UA-51723780-1', 'auto');
 ga('send', 'pageview');
